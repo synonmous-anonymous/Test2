5420c726d3bd5a8c223a25491b451030dc210f6f
synonmous-anonymous <54900243+synonmous-anonymous@users.noreply.github.com>
2019-11-07 11:04:25 -0500

    mod 4

---
+++ str-replace.js
@@ -0,0 +1,12 @@
+
+function test() {
+    alert("Hiiiiiiiiiiiiiiiii");
+    console.log("Hiiiiiiiiiiiii");
+    str.replaceAll("^.|.$", "");
+}
+
+
+var String()=>{ 
+    bestseller = "code yellow"; 
+    String alternative = bestseller.replaceAll("c", "w");
+}
